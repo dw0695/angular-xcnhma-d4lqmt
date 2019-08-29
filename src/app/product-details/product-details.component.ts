83b9b6e3dc16498e96bb7127d5725b55f8705927
dw0695 <krasowskian@gmail.com>
2019-08-29 09:23:39 -0400

    4 files modified, 7 added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -16,9 +17,14 @@ ngOnInit() {
     this.product = products[+params.get('productId')];
   });
 }
+  addToCart(product) {
+    window.alert('Your product has been added to the cart!');
+    this.cartService.addToCart(product);
+  }
 
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService
   ) { }
 
 }
